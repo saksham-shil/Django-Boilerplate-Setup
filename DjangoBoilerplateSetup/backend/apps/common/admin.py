ebdaea5e5b8fef5e780a622a16da6a885909dfe8
saksham-shil <saksham.shil@velsof.com>
2025-09-22 21:55:30 +0530

    Delete DjangoBoilerplateSetup directory

--- DjangoBoilerplateSetup/backend/apps/common/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .audit.models import AuditLog
-
-# Register your models here.
-admin.site.register(AuditLog) 
--- DjangoBoilerplateSetup/backend/apps/common/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-app_name = "common"
-urlpatterns = [
-    path("api/check", views.check, name="check"),
-]
